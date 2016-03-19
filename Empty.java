2205d311c7b9c494473e69bb9f47d3c78a39b431
aleks2510 <aleks.2510@hotmail.com>
2016-03-18 21:13:49 -0600

    Added files via upload
    FINAL

---
+++ Empty.java
@@ -0,0 +1,12 @@
+package game;
+//This class creates an object of the superclass PipeSuperClass called Empty
+//All of the sides are closed.
+class Empty extends PipeSuperClass {
+	
+	Empty(){
+		setSide1(false);
+		setSide2(false);
+		setSide3(false);
+		setSide4(false);
+	}
+}
