5157dd9920e1929b749c1ee541c99ab8a04b9b9f
aleks2510 <aleks.2510@hotmail.com>
2016-03-18 21:12:44 -0600

    Update PipeEnding.java
    FINAL

--- PipeEnding.java
+++ PipeEnding.java
@@ -1,15 +1,13 @@
 package game;
-
+//This class creates an object of the superclass PipeSuperClass called PipeEnding.
+//the sides are positioned in the cardinal way.
 public class PipeEnding extends PipeSuperClass{
 
-	boolean side1 = false;
-	boolean side2 = false;
-	boolean side3 = false;
-	boolean side4 = true;
-	
-	
 	PipeEnding(){
-		
+		setSide1(false);
+		setSide2(false);
+		setSide3(false);
+		setSide4(true);
 	}
 	
 }
