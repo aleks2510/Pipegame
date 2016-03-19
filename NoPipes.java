c0cf8401a0a9ca922c707687e56b255f0c3afdd5
aleks2510 <aleks.2510@hotmail.com>
2016-03-18 21:13:03 -0600

    Update NoPipes.java
    FINAL

--- NoPipes.java
+++ NoPipes.java
@@ -1,14 +1,14 @@
 package game;
-
+//This class creates an object of the superclass PipeSuperClass called NoPipe
+//the sides are positioned in the cardinal way.
 public class NoPipes extends PipeSuperClass{
 	
-	boolean side1 = false;
-	boolean side2 = false;
-	boolean side3 = false;
-	boolean side4 = false;
 	
 	NoPipes(){
-		
+		setSide1(false);
+		setSide2(false);
+		setSide3(false);
+		setSide4(false);
 	}
 	
 
