aac950945308f4b8ebaf7879b409c7124f709459
aleks2510 <aleks.2510@hotmail.com>
2016-03-18 21:12:28 -0600

    Update PipeES.java
    FINAL

--- PipeES.java
+++ PipeES.java
@@ -1,15 +1,15 @@
 package game;
-
-public class PipeES extends PipeSuperClass{
-	
-	boolean side1 = false;
-	boolean side2 = true;
-	boolean side3 = true;
-	boolean side4 = false;
+//This class creates an object of the superclass PipeSuperClass called PipeES
+//the sides are positioned in the cardinal way.
+public class PipeES extends PipeSuperClass{		
 	
 	PipeES(){
-		
+		setSide1(false);
+		setSide2(true);
+		setSide3(true);
+		setSide4(false);
 	}
 	
 
 }
+
