89fe66b3c2a7a806f4d0be27aeb17f6dcc277f9d
aleks2510 <aleks.2510@hotmail.com>
2016-03-18 21:11:07 -0600

    Update PipeNW.java
    FINAL

--- PipeNW.java
+++ PipeNW.java
@@ -1,14 +1,16 @@
+//This class creates an object of the superclass PipeSuperClass called PipeNW
+//the sides are positioned in the cardinal way.
 package game;
 
 public class PipeNW extends PipeSuperClass{
-	boolean side1 = true;
-	boolean side2 = false;
-	boolean side3 = false;
-	boolean side4 = true;
-	
+
 	
 	PipeNW(){
-		
+
+		setSide1(true);
+		setSide2(false);
+		setSide3(false);
+		setSide4(true);
 	}
 	
 	
