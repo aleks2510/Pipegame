7befd5d29099c1511eeeb1c1889a662c409ab247
aleks2510 <aleks.2510@hotmail.com>
2016-03-18 21:10:27 -0600

    Update PipeStart.java
    FINAL

--- PipeStart.java
+++ PipeStart.java
@@ -1,14 +1,13 @@
 package game;
-
+//This class creates an object of the superclass PipeSuperClass called PipeStart
+//the sides are positioned in the cardinal way.
 public class PipeStart extends PipeSuperClass{
-
-	boolean side1 = false;
-	boolean side2 = true;
-	boolean side3 = false;
-	boolean side4 = false;
 	
 	PipeStart(){
-		
+		setSide1(false);
+		setSide2(true);
+		setSide3(false);
+		setSide4(false);
 	}
 	
 }
