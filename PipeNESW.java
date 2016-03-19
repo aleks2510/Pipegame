90ef9a2d5e06d9153b186d5d30a53097f9dec8ea
aleks2510 <aleks.2510@hotmail.com>
2016-03-18 21:11:33 -0600

    Update PipeNESW.java
    FINAL

--- PipeNESW.java
+++ PipeNESW.java
@@ -1,13 +1,14 @@
 package game;
-
+//This class creates an object of the superclass PipeSuperClass called PipeNESW which it is a cross
+//the sides are positioned in the cardinal way.
 public class PipeNESW extends PipeSuperClass{
 
-	boolean side1 = true;
-	boolean side2 = true;
-	boolean side3 = true;
-	boolean side4 = true;
 	
 	PipeNESW(){
+		setSide1(true);
+		setSide2(true);
+		setSide3(true);
+		setSide4(true);
 		
 	}
 	
